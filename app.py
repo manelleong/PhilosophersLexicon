4c283b35160a181bd728d945d06c6fc2b9bb8a0f
manelleong <mleong@wm.edu>
2024-04-24 15:23:13 -0400

    Got fake graph to work Got a test graph with the selected philosophers included to display when submitted

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request
+import plotly.express as px
+import plotly
+import json
+from services import topic_modeling
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+
     return render_template('index.html')
 
 @app.route('/submit', methods = ['POST'])
@@ -12,8 +17,12 @@ def submit():
 
     selected_options = [request.form[philosopher] for philosopher in philosophers if request.form.get(philosopher)]
 
-    print(selected_options)
+    if not selected_options:
+        return render_template('index.html')
 
-    return render_template('index.html')
+    fig = topic_modeling.makeGraph(selected_options)
+    graphjson = fig.to_json()
+
+    return render_template('index.html', graphJSON = graphjson)
 
 app.run(host = "0.0.0.0", port = 80)
